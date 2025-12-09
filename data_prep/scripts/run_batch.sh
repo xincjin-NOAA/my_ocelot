df03505d01af16872c29eaac4a32277c69a57399
Xin Jin <Xin.C.Jin@noaa.gov>
2025-12-09 14:48:50 +0000

    minor

--- data_prep/scripts/run_batch.sh
+++ data_prep/scripts/run_batch.sh
@@ -14,5 +14,8 @@ source /scratch3/NCEPDEV/da/Xin.C.Jin/my_projects/ocelot/data/env.sh
 source /scratch3/NCEPDEV/da/Xin.C.Jin/my_projects/ocelot/data/venv/bin/activate
 #srun -n 24 python gen_data.py 2024-01-01 2024-12-31 $1 zarr
 #python gen_data.py 2024-01-01 2024-04-30 raw_radiosonde zarr
-python gen_data.py 2024-01-01 2024-01-03 amsua cycle_parquet
+#python gen_data.py -p 2024-03-23 2024-12-31 atms cycle_parquet
+#python gen_data.py 2024-01-01 2024-12-31 raw_surface_obs cycle_parquet
+python gen_data.py 2024-01-01 2024-12-31 raw_surface_obs zarr
+#python gen_data.py  -b  --slurm_account da-cpu 2024-01-01 2024-04-30 amsua cycle_parquet
 
