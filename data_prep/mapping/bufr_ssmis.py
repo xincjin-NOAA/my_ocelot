cf7e06624f50ee095d8a67b7c218e13138a6edc6
rmclaren <ronald.mclaren@noaa.gov>
2025-12-10 11:30:08 -0500

    Add SEVIRI and SSMIS (#71)
    Adds SEVIRI and SSMIS data types to data prep.

--- data_prep/mapping/bufr_ssmis.py
+++ data_prep/mapping/bufr_ssmis.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import numpy as np
 
 from bufr.obs_builder import ObsBuilder, add_main_functions, map_path
 
@@ -9,6 +10,31 @@ MAPPING_PATH = map_path('bufr_ssmis.yaml')
 class BufrSsmisObsBuilder(ObsBuilder):
     def __init__(self):
         super().__init__(MAPPING_PATH, log_name=os.path.basename(__file__))
+
+    def make_obs(self, comm, input_path) -> bufr.DataContainer:
+        container = super().make_obs(comm, input_path)
+
+        latitude1 = container.get('latitude1')
+        latitude2 = container.get('latitude2')
+
+        isAscending = np.where(latitude2 > latitude1, 1, -1).astype(np.int32)
+        container.add('satelliteAscendingFlag', isAscending, ["*"])
+
+        return container
+
+    def _make_description(self):
+        description = super()._make_description()
+
+        description.add_variables([
+            {
+                'name': "satelliteAscendingFlag",
+                'source': 'satelliteAscendingFlag',
+                'longName': "Satellite Ascending Flag",
+                'units': "boolean",
+            }
+        ])
+
+        return description
 
 
 # Add main functions create_obs_file or create_obs_group
