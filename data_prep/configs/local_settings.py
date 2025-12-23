a25c86724b9dba2573299a3442651fa28b0421d0
Xin Jin <Xin.C.Jin@noaa.gov>
2025-12-23 03:51:57 +0000

    local_settings.py

---
+++ data_prep/configs/local_settings.py
@@ -0,0 +1,25 @@
+
+# Path to the configuration yaml file for the BUFR tank
+# (example: '/install_dir/spoc/tank/conf/hera_test_tank.yaml')
+BUFR_TANK_YAML = '/scratch3/NCEPDEV/da/Xin.C.Jin/git/my_ocelot/data_prep/configs/ursa.yaml'
+
+# Path to the root directory for the BUFR tank
+# (example: '/data_dir/ops/prod/dcom')
+TANK_PATH = '/scratch3/NCEPDEV/global/role.glopara/dump' 
+
+# The datetime format string for the subdirecotries in the tank 
+# (see docs for python datetime object)
+DATETIME_DIR_FORMAT = 'gdas.%Y%m%d'
+
+# Path to the directory that holds the BUFR mapping files
+# (example: '/install_dir/src/spoc/tank/mapping')
+MAPPING_FILE_DIR = '/scratch3/NCEPDEV/da/Xin.C.Jin/git/my_ocelot/data_prep/mapping'
+
+OUTPUT_PATH = '/scratch3/NCEPDEV/stmp/Xin.C.Jin/data/ocelot/data_v6/global/dev'
+
+BUFR_TABLE_DIR = '/scratch3/NCEPDEV/da/Ronald.McLaren/src/NCEPLIBS-bufr/tables'
+
+# The range of latitude and longitude for the continental US
+#LAT_RANGE = (24, 51)
+#LON_RANGE = (-115, -74)
+
